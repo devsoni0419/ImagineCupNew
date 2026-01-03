7a57ee93734b7e61fb8ba4e7e746c47fc8a8b144
devsoni0419 <devsoni0419@gmail.com>
2026-01-03 15:06:24 +0530

    'keys'

---
+++ keys.py
@@ -0,0 +1,23 @@
+AZURE_CONFIG = {
+
+    "Azure_Subscription": {
+        "AZURE_SUBSCRIPTION_ID": "Azure subscription identifier"
+    },
+
+    "Azure_Speech": {
+        "AZURE_SPEECH_KEY": "Speech service API key",
+        "AZURE_SPEECH_REGION": "Speech service region"
+    },
+
+    "Azure_OpenAI": {
+        "AZURE_OPENAI_KEY": "Azure OpenAI API key",
+        "AZURE_OPENAI_ENDPOINT": "Azure OpenAI resource endpoint"
+    },
+
+    "Azure_Machine_Learning": {
+        "AZURE_ML_WORKSPACE_NAME": "Azure ML workspace name",
+        "AZURE_ML_RESOURCE_GROUP": "Resource group containing the ML workspace",
+        "AZURE_ML_SUBSCRIPTION_ID": "Subscription ID for Azure ML"
+    }
+
+}
